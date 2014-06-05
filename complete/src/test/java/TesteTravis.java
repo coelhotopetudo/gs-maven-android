8dc61b7bb246219bc178ab38708837ee0ddc66b6
coelhotopetudo <coelhotopetudo@gmail.com>
2014-06-05 00:00:43 -0300

    tt

---
+++ complete/src/test/java/TesteTravis.java
@@ -0,0 +1,15 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteTravis {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		System.out.println("Coelho rocks");
+	}
+
+}
